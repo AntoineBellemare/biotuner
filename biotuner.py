4b05f6ff45200b1a348ed547460b357004b76939
Antoine Bellemare <antoine.bellemare9@gmail.com>
2021-05-24 09:17:36 -0400

    biotuner 2d reformatting

--- biotuner.py
+++ biotuner.py
@@ -94,6 +94,7 @@ def harmonic_fit(peaks, n_harm = 10, bounds = 1, function = 'mult'):
 #EXTENDED PEAKS from restrictions#
 
 def consonance_peaks (peaks, limit):
+    print(peaks)
     from fractions import Fraction
     consonance_ = []
     peaks2keep = []
@@ -328,9 +329,12 @@ def peaks_to_metrics (peaks, n_harm = 10):
     peaks = list(peaks)
     metrics = {'cons' : 0, 'euler' : 0, 'tenney': 0, 'harm_fit': 0}   
     metrics['harm_fit'] = len(harmonic_fit(peaks, n_harm = n_harm))
+
     a, b, c, metrics['cons'] = consonance_peaks (peaks, 0.1)
-    metrics['euler'] = euler(*peaks)
-    metrics['tenney'] = tenneyHeight(peaks)
+    peaks_highfreq  = [int(p*1000) for p in peaks]
+    
+    metrics['euler'] = euler(*peaks_highfreq)
+    metrics['tenney'] = tenneyHeight(peaks_highfreq) 
     metrics_list = []
     for value in metrics.values():
         metrics_list.append(value)
@@ -508,6 +512,7 @@ def diss_curve (freqs, amps, denom=1000, max_ratio=2, consonance = True, method 
     intervals.append((2, 1))
     ratios = [i[0]/i[1] for i in intervals]
     ratios_sim = [np.round(r, 2) for r in ratios] #round ratios for similarity measures of harmonic series
+    print(ratios_sim)
     dyad_sims = ratios2harmsim(ratios[:-1]) # compute dyads similarities with natural harmonic series
     dyad_sims
     a = 1
@@ -716,115 +721,7 @@ def compute_peaks_ts (data, peaks_function = 'EMD', FREQ_BANDS = None, precision
     return peaks, amps
 
 
-'''BIOTUNER 2D'''
 
-
-def surrogate_signal(data, surr_type = 'pink', low_cut = 0.5, high_cut = 150, sf = 1000):
-    if surr_type == 'AAFT':
-        indexes = [x for x in range(len(data))]
-        data = np.stack((data, indexes))
-        data = AAFT_surrogates(Surrogates, data)
-        data = data[0]
-        data = butter_bandpass_filter(data, low_cut, high_cut, sf, 4)
-    if surr_type == 'phase':
-        len_data = len(data)
-        data = phaseScrambleTS(data)
-        data = butter_bandpass_filter(data[0:len_data], low_cut, high_cut, sf, 4)
-    if surr_type == 'shuffle':
-        np.random.shuffle(data)
-    if surr_type == 'white':
-        beta = 0 
-    if surr_type == 'pink':
-        beta  = 1
-    if surr_type == 'brown':
-        beta = 2
-    if surr_type == 'blue':
-        beta  = -1
-    if surr_type == 'white' or surr_type == 'pink' or surr_type == 'brown' or surr_type == 'blue':
-        data = cn.powerlaw_psd_gaussian(beta, len(data))
-        data = butter_bandpass_filter(data, low_cut, high_cut, sf, 4)
-    return data
-    
-def surrogate_signal_matrices(data, surr_type = 'pink', low_cut = 0.5, high_cut = 150, sf = 1000):
-    if np.ndim(data) == 2:
-        for i in range(len(data)):
-            data[i] = surrogate_signal(data[i], surr_type = surr_type, low_cut = low_cut, high_cut = high_cut, sf = sf)
-    if np.ndim(data) == 3:
-        for i in range(len(data)):
-            for j in range(len(data[i])):
-                data[i][j] = surrogate_signal(data[i][j], surr_type = surr_type, low_cut = low_cut, high_cut = high_cut, sf = sf)
-    return data
-def compute_peaks_matrices (data, peaks_function = 'EMD', precision = 0.25, sf = 1000, max_freq = 80, save = True, suffix = 'default'):
-    if np.ndim(data) == 2:
-        peaks_tot = []
-        amps_tot = []
-        for i in range(len(data)):
-            peaks, amps = compute_peaks_ts(data[i], peaks_function = peaks_function, FREQ_BANDS = None, precision = precision, sf = sf, max_freq = max_freq) 
-            peaks_tot.append(peaks)
-            amps_tot.append(amps)
-        peaks_total = np.array(peaks_tot)
-        amps_total = np.array(amps_tot)
-        if save == True:
-            np.save('peaks_{}_{}'.format(peaks_function, suffix), peaks_total)
-            np.save('amps_{}_{}'.format(peaks_function, suffix), amps_total)
-    if np.ndim(data) == 3:
-        peaks_tot = []
-        amps_tot = []
-        for i in range(len(data)):
-            peaks_temp = []
-            amps_temp = []
-            for j in range(len(data[i])):
-                peaks, amps = compute_peaks_ts(data[i][j], peaks_function = peaks_function, FREQ_BANDS = None, precision = precision, sf = sf, max_freq = max_freq)
-                peaks_temp.append(peaks)
-                amps_temp.append(amps)
-            peaks_tot.append(peaks_temp)
-            amps_tot.append(amps_temp)
-        peaks_total = np.array(peaks_tot)
-        amps_total = np.array(amps_tot)
-        if save == True:
-            np.save('peaks_{}_{}'.format(peaks_function, suffix), peaks_total)
-            np.save('amps_{}_{}'.format(peaks_function, suffix), amps_total)
-    return peaks_total, amps_total
-
-def compute_peaks_surrogates (data, conditions, peaks_function = 'EMD', precision = 0.25, sf = 1000, max_freq = 80, low_cut = 0.5, high_cut = 150, save = False):
-    peaks_tot = []
-    amps_tot = []
-    for e, c in enumerate(conditions):
-        print('Condition (', e+1, 'of', len(conditions), '):', c)
-        if c == 'og_data':
-            peaks, amps = compute_peaks_matrices(data, peaks_function, precision, sf, max_freq)
-        if c != 'og_data':
-            data = surrogate_signal_matrices(data, surr_type = c, low_cut = low_cut, high_cut = high_cut, sf = sf)
-            peaks, amps = compute_peaks_matrices(data, peaks_function, precision, sf, max_freq, save = False, suffix = 'default')
-        peaks_tot.append(peaks)
-        amps_tot.append(amps)
-    peaks_total = np.array(peaks_tot)
-    amps_total = np.array(amps_tot)
-        
-    return peaks_total, amps_total
-
-def peaks_to_metrics_matrices (peaks):
-    cons = []
-    euler = []
-    tenney = []
-    harm_fit = []
-    if np.ndim(peaks) == 3:
-        for i in range(len(peaks)):
-            cons_temp = []
-            euler_temp = []
-            tenney_temp = []
-            harm_fit_temp = []
-            for j in range(len(peaks[i])):
-                metrics, metrics_list = peaks_to_metrics(peaks[i][j], 10)
-                cons_temp.append(metrics_list[0])
-                euler_temp.append(metrics_list[1])
-                tenney_temp.append(metrics_list[2])
-                harm_fit_temp.append(metrics_list[3])
-            cons.append(cons_temp)
-            euler.append(euler_temp)
-            tenney.append(tenney_temp)
-            harm_fit.append(harm_fit_temp)
-    return np.array([cons, euler, tenney, harm_fit])
 
 
 '''OLD sanity_check_code'''
