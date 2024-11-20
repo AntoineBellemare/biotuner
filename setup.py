75166c2991188ed8490574e0406d2837e20b0161
AntoineBellemare <antoine.bellemare9@gmail.com>
2024-11-20 00:20:14 -0500

    removed empty line and fix requirements import

--- setup.py
+++ setup.py
@@ -1,22 +1,32 @@
 from setuptools import setup
+import os
+
+# Dynamically locate requirements.txt
+current_dir = os.path.abspath(os.path.dirname(__file__))
+requirements_path = os.path.join(current_dir, 'requirements.txt')
+
+if os.path.exists(requirements_path):
+    with open(requirements_path) as f:
+        install_requires = f.read().splitlines()
+else:
+    install_requires = []
 
 setup(
-  name = 'biotuner',         # How you named your package folder (MyLib)
-  packages = ['biotuner'],   # Chose the same as "name"
-  version = '0.0.12',      # Start with a small number and increase it with every change you make
-  license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
-  description = 'Time series harmonic analysis for adaptive tuning systems and microtonal exploration',   # Give a short description about your library
-  author = 'Antoine Bellemare',                   # Type in your name
-  author_email = 'antoine.bellemare9@gmail.com',      # Type in your E-Mail
-  url = 'https://github.com/antoinebellemare/biotuner',   # Provide either the link to your github or to your website
-  #download_url = 'https://github.com/AntoineBellemare/biotuner/archive/refs/tags/v0.0.11.tar.gz',    # I explain this later on
-  keywords = ['biosignal', 'harmony', 'tuning', 'eeg', 'microtonality', 'music', 'time series'],   # Keywords that define your package best
-  install_requires= open('requirements.txt').read().splitlines(),  # I get to this in a second,
+  name='biotuner',
+  packages=['biotuner'],
+  version='0.0.12',
+  license='MIT',
+  description='Time series harmonic analysis for adaptive tuning systems and microtonal exploration',
+  author='Antoine Bellemare',
+  author_email='antoine.bellemare9@gmail.com',
+  url='https://github.com/antoinebellemare/biotuner',
+  keywords=['biosignal', 'harmony', 'tuning', 'eeg', 'microtonality', 'music', 'time series'],
+  install_requires=install_requires,  # Use the dynamically loaded install_requires
   classifiers=[
-    'Development Status :: 3 - Alpha',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
-    'Intended Audience :: Developers',      # Define that your audience are developers
+    'Development Status :: 3 - Alpha',
+    'Intended Audience :: Developers',
     'Topic :: Software Development :: Build Tools',
-    'License :: OSI Approved :: MIT License',   # Again, pick a license
+    'License :: OSI Approved :: MIT License',
     'Programming Language :: Python :: 3.6',
     'Programming Language :: Python :: 3.7',
     'Programming Language :: Python :: 3.8',
