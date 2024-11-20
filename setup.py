f522607b4f70093c025d3ee13aa62dcdf361c873
AntoineBellemare <antoine.bellemare9@gmail.com>
2024-11-20 00:13:24 -0500

    0.12

--- setup.py
+++ setup.py
@@ -3,13 +3,13 @@ from setuptools import setup
 setup(
   name = 'biotuner',         # How you named your package folder (MyLib)
   packages = ['biotuner'],   # Chose the same as "name"
-  version = '0.0.11',      # Start with a small number and increase it with every change you make
+  version = '0.0.12',      # Start with a small number and increase it with every change you make
   license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
   description = 'Time series harmonic analysis for adaptive tuning systems and microtonal exploration',   # Give a short description about your library
   author = 'Antoine Bellemare',                   # Type in your name
   author_email = 'antoine.bellemare9@gmail.com',      # Type in your E-Mail
   url = 'https://github.com/antoinebellemare/biotuner',   # Provide either the link to your github or to your website
-  download_url = 'https://github.com/AntoineBellemare/biotuner/archive/refs/tags/v0.0.11.tar.gz',    # I explain this later on
+  #download_url = 'https://github.com/AntoineBellemare/biotuner/archive/refs/tags/v0.0.11.tar.gz',    # I explain this later on
   keywords = ['biosignal', 'harmony', 'tuning', 'eeg', 'microtonality', 'music', 'time series'],   # Keywords that define your package best
   install_requires= open('requirements.txt').read().splitlines(),  # I get to this in a second,
   classifiers=[
@@ -17,9 +17,6 @@ setup(
     'Intended Audience :: Developers',      # Define that your audience are developers
     'Topic :: Software Development :: Build Tools',
     'License :: OSI Approved :: MIT License',   # Again, pick a license
-    'Programming Language :: Python :: 3',      #Specify which pyhton versions that you want to support
-    'Programming Language :: Python :: 3.4',
-    'Programming Language :: Python :: 3.5',
     'Programming Language :: Python :: 3.6',
     'Programming Language :: Python :: 3.7',
     'Programming Language :: Python :: 3.8',
