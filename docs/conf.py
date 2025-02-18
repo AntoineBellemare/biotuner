c70bd8928b8f020a7d86ea35fc1942c8e43f43ee
Antoine Bellemare <antoine.bellemare9@gmail.com>
2025-02-17 20:37:54 -0500

    trigger action

--- docs/conf.py
+++ docs/conf.py
@@ -13,8 +13,8 @@ import sys
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #
-sys.path.insert(0, os.path.abspath('.'))
-#sys.path.insert(0, os.path.abspath("../"))
+sys.path.insert(0, os.path.abspath("."))
+# sys.path.insert(0, os.path.abspath("../"))
 
 
 # -- Project information -----------------------------------------------------
@@ -30,6 +30,7 @@ def find_author():
 project = "Biotuner"
 copyright = f"2023–{datetime.datetime.now().year}"
 author = 'Antoine Bellemare & François Lespinasse. This documentation and code are licensed under the <a href="https://opensource.org/licenses/MIT">MIT License</a>.'
+
 
 # The short X.Y version.
 def find_version():
@@ -48,19 +49,22 @@ release = version
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = ["nbsphinx", "sphinx_nbexamples",
-              "jupyter_sphinx",
-              "sphinx.ext.autodoc",
-              "sphinx.ext.doctest",
-              "sphinx.ext.todo",
-              "sphinx.ext.napoleon",
-              "sphinx.ext.autosectionlabel",
-              "sphinx.ext.viewcode",
-              "IPython.sphinxext.ipython_console_highlighting",
-              "IPython.sphinxext.ipython_directive",
-              "sphinxemoji.sphinxemoji",
-              "sphinx_copybutton",
-              "myst_nb",]
+extensions = [
+    "nbsphinx",
+    "sphinx_nbexamples",
+    "jupyter_sphinx",
+    "sphinx.ext.autodoc",
+    "sphinx.ext.doctest",
+    "sphinx.ext.todo",
+    "sphinx.ext.napoleon",
+    "sphinx.ext.autosectionlabel",
+    "sphinx.ext.viewcode",
+    "IPython.sphinxext.ipython_console_highlighting",
+    "IPython.sphinxext.ipython_directive",
+    "sphinxemoji.sphinxemoji",
+    "sphinx_copybutton",
+    "myst_nb",
+]
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ["_templates"]
@@ -86,7 +90,7 @@ napoleon_use_ivar = False
 napoleon_use_rtype = False
 add_module_names = False  #  If true, the current module name will be prepended to all description
 
-templates_path = ['_templates']
+templates_path = ["_templates"]
 exclude_patterns = []
 
 # nbsphinx_execute = 'examples/**/*.ipynb'
@@ -104,44 +108,43 @@ nb_execution_raise_on_error = True
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-#html_theme = 'alabaster'
-#html_static_path = ['_static']
-#html_theme = 'sphinx_rtd_theme'
-html_theme = 'sphinx_book_theme'
-#html_theme = 'pydata_sphinx_theme'
+# html_theme = 'alabaster'
+# html_static_path = ['_static']
+# html_theme = 'sphinx_rtd_theme'
+html_theme = "sphinx_book_theme"
+# html_theme = 'pydata_sphinx_theme'
 # -- Options for HTML output -------------------------------------------------
 
 html_favicon = "img/favicon.ico"
 html_logo = "img/logo.png"
-autoclass_content = 'both'
+autoclass_content = "both"
 
 
 # Add autodoc options
 autodoc_default_options = {
-    'members': True,
-    'undoc-members': False,
-    'private-members': False,
-    'special-members': False,
-    'show-inheritance': True,
-    'member-order': 'bysource',
-    'exclude-members': '__weakref__, __module__',
+    "members": True,
+    "undoc-members": False,
+    "private-members": False,
+    "special-members": False,
+    "show-inheritance": True,
+    "member-order": "bysource",
+    "exclude-members": "__weakref__, __module__",
 }
 
 latex_elements = {
-    'preamble': r'''
+    "preamble": r"""
     \usepackage{tocloft}
     \renewcommand{\cftchapleader}{\cftdotfill{\cftdotsep}}
     \renewcommand{\cfttoctitlefont}{\hfill\Large\bfseries}
     \renewcommand{\cftaftertoctitle}{\hfill}
-    ''',
+    """,
 }
 html_static_path = ["_static"]
 
 
-
-#git push origin --delete docs_build
-#git checkout -b docs_build
-#sphinx-build -b html docs docs/_build/html
-#git add docs/_build/html
-#git commit -m "Update documentation"
-#git push --set-upstream origin docs_build
+# git push origin --delete docs_build
+# git checkout -b docs_build
+# sphinx-build -b html docs docs/_build/html
+# git add docs/_build/html
+# git commit -m "Update documentation"
+# git push --set-upstream origin docs_build
